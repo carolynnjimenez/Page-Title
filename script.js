fc7337e2f8f531b38228f00cf701ea8800e00fe7
carolynnjimenez <40403222+carolynnjimenez@users.noreply.github.com>
2018-12-07 09:32:33 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,97 @@
+$("#Help").click(function() {
+ $("#img2").fadeToggle();
+ $("#img1").fadeToggle();
+ $("#p1").fadeToggle();
+ $("2").fadeToggle();
+   $("#p2").fadeToggle();
+   $("#Help").fadeToggle();
+   
+});
+
+$("#img2").click(function() {
+ $("#img3").fadeToggle();
+ $("#p2").fadeToggle();
+ $("#img2").fadeToggle();
+  $("#p3").fadeToggle();
+});
+
+$("#img3").dblclick(function() {
+ $("#img3").fadeToggle();
+  $("#p3").fadeToggle();
+  $("#p4").fadeToggle();
+  $("#li").fadeToggle();
+  });
+  
+  $("#Real").hover(function() {
+       $("#Found").fadeIn();
+  });
+      $("#Found").click(function() {
+          $("ul").fadeToggle();
+           $("h1").fadeToggle();
+              $("#p4").fadeOut();
+               $("#p5").fadeToggle();
+             
+      });
+      
+       $("#p5").click(function() {
+      $("#p5").fadeToggle();
+      $("#img4").fadeToggle();
+        $("#2").fadeToggle();
+      $("#4").fadeToggle();
+       $("#p6").fadeToggle();
+        $("#math").fadeToggle();
+        $("#22").fadeToggle();
+       });
+       
+       $("#22").click(function() {
+  $("#response").text("Idiot");
+  });
+      
+         
+$("#4").click(function() {
+ $("#p6").fadeToggle();
+  $("#img4").fadeToggle();
+   $("#math").fadeToggle();
+   $("#4").fadeToggle();
+   $("#22").fadeToggle();
+   $("#p8").fadeToggle();
+   $("#img7").fadeToggle();
+    $("#img8").fadeToggle();
+ 
+ 
+ $("#4").click(function() {
+     $("#img7").fadeToggle();
+ });
+});
+  
+  
+  $("#img5").click(function() {
+    $("#img5").fadeToggle();   
+    $("#p9").fadeToggle(); 
+    $("#img6").fadeToggle();
+    
+  }); 
+  
+  $("#img8").dblclick(function() {
+      $("#img5").fadeToggle();
+      $("#img7").fadeToggle();
+      $("#p8").fadeToggle();
+      $("#img8").fadeToggle();
+      $("#p9").fadeToggle();  
+      
+  }); 
+  
+   $("#img6").dblclick(function() {
+       $("#GOD").fadeToggle();
+       $("#img6").fadeToggle();
+       $("#Stan").fadeToggle();
+   });    
+      
+      
+      
+      
+      
+      
+      
+      
+      
